8b378ef09aff26e269902983b06205e83cd13633
Khaendall <khaendall@gmail.com>
2026-02-14 08:43:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -27,16 +27,33 @@ calc();
 
 function calc(){
 
+const manualMode = dataSource.value==="manual";
+
+let B2=0;
+let C2=0;
+
+if(!manualMode){
 const heroName=hero.value;
 const heroData=DB[heroName]||{base:0,prow:0};
 
-const B2=heroData.base;
-const C2=heroData.prow;
+B2=heroData.base;
+C2=heroData.prow;
+
+baseSPD.value=B2;
+prowSPD.value=C2;
+
+baseSPD.disabled=true;
+prowSPD.disabled=true;
+}else{
+B2=Number(baseSPD.value)||0;
+C2=Number(prowSPD.value)||0;
+
+baseSPD.disabled=false;
+prowSPD.disabled=false;
+}
+
 const D2=num("shell");
 const E2=num("set");
-
-baseSPD.innerText=B2;
-prowSPD.innerText=C2;
 
 const slotIDs=["mask","transistor","wristwheel","core1","core2","core3"];
 const G8=slotIDs.map(id=>num(id)).reduce((a,b)=>a+b,0);
@@ -45,14 +62,12 @@ slotSum.innerText=G8;
 
 const G10=num("manual");
 
-// BLOCKING
 if(G8>0) manual.disabled=true;
 else manual.disabled=false;
 
 if(G10>0) slotIDs.forEach(id=>document.getElementById(id).disabled=true);
 else slotIDs.forEach(id=>document.getElementById(id).disabled=false);
 
-// TOTAL SPEED
 let A4;
 if(G8>0)
 A4=B2*(1+E2/100)+C2+D2+40+G8;
@@ -61,7 +76,6 @@ A4=B2*(1+E2/100)+C2+D2+40+G10;
 
 totalSPD.innerText=Math.round(A4);
 
-// ENEMY SPEED
 const B7=num("myspd");
 const C7=mytm.value;
 const D7=histm.value;
@@ -72,7 +86,6 @@ else if(D7!=="") A7=B7*(Number(D7)/100);
 
 enemySPD.innerText=A7?Math.round(A7):"";
 
-// ENEMY MODULE
 const E7=num("enemymanual");
 
 let A9="";
@@ -83,7 +96,6 @@ A9=E7-(B2*(1+E2/100))-C2-D2-40;
 
 enemyModule.innerText=A9?Math.round(A9):"";
 
-// HIDE TOTAL SPEED IN SCENARIO 2/3
 if(A7!=="" || E7!==0)
 totalSPD.style.visibility="hidden";
 else
